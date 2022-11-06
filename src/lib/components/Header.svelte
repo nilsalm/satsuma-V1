44a8fcc6f50b0b08900237b6351ae92e20056232
Nils Weber <ntweber75@gmail.com>
2022-11-06 23:44:46 +0100

    ✨ category-based rendering

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  let user = { loggedIn: true };
+  function toggle() {
+    user.loggedIn = !user.loggedIn;
+  }
+</script>
+
+<div class="flex flex-row">
+  <div class="text-xl">Satsuma</div>
+
+  {#if user.loggedIn}
+    <button on:click={toggle}> Log out </button>
+    <div />
+  {/if}
+
+  {#if !user.loggedIn}
+    <button on:click={toggle}> Log in </button>
+  {/if}
+</div>
---
+++ src/lib/models/CategoryModel.ts
@@ -0,0 +1,15 @@
+export type Category = {
+  id: string,
+  name: string,
+}
+
+export enum CategoryEnum {
+  Fruit,
+  Vegetable,
+  Cheese,
+  Meat,
+  Dairy,
+  Household,
+  Pasta,
+  Preserved,
+}
