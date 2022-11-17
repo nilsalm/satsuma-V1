dd14f931e040306c2ddc8e407f06c290220c3ddf
Nils Alm <nils.alm@prototyp.se>
2022-11-17 18:09:45 +0100

    ✨ inc/dec buttons on item

--- src/routes/lists/[listId]/ItemComponent.svelte
+++ src/routes/lists/[listId]/ItemComponent.svelte
@@ -10,24 +10,42 @@
 		item.picked = !item.picked;
 		dispatch('update', item);
 	}
-	function more() {
+	function increase() {
 		item.quantity += 1;
 		dispatch('update', item);
 	}
-	function less() {
+	function decrease() {
 		item.quantity -= 1;
 		dispatch('update', item);
 	}
 </script>
 
 <div
-	class="bg-green-400 py-2 px-4 rounded-sm mb-1 mx-4"
-	on:dragleave={more}
-	on:click={tickOff}
-	on:keypress={tickOff}
+	class="bg-gradient-to-r from-yellow-400 to-orange-500 border-b-2 border-gray-200 py-2 px-4 rounded-sm mx-4"
 >
-	<div class="flex flex-row">
-		<div class="w-8">{item.quantity}</div>
-		<div class="font-semibold">{item.name}</div>
+	<div class="flex flex-row justify-between">
+		<div class="flex flex-row">
+			<div class="w-8">{item.quantity}</div>
+			<div class="pr-10 font-semibold whitespace-nowrap" on:click={tickOff} on:keypress={tickOff}>
+				{item.name}
+			</div>
+		</div>
+		<div class="w-full" on:click={tickOff} on:keypress={tickOff} />
+		<div class="flex flex-row">
+			<div
+				class="bg-gray-400 text-white font-semibold px-2 mx-1 hover:bg-yellow-400 active:border-yellow-200 border-2  border-gray-300 rounded"
+				on:click={decrease}
+				on:keypress={decrease}
+			>
+				-
+			</div>
+			<div
+				class="bg-gray-400 text-white font-semibold px-2 mx-1 hover:bg-yellow-400 active:border-yellow-200 border-2  border-gray-300 rounded"
+				on:click={increase}
+				on:keypress={increase}
+			>
+				+
+			</div>
+		</div>
 	</div>
 </div>
