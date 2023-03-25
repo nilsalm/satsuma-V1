5f76275bbd8d603c7c1fd89ab3d80bb10604fe64
Nils Alm <nils.alm@prototyp.se>
2023-03-25 17:16:59 +0100

    Add edit and delete functionality for lists

---
+++ src/routes/lists/[listId]/edit/+page.server.ts
@@ -0,0 +1,50 @@
+import { error, redirect, type Actions } from '@sveltejs/kit';
+
+export const load = async ({ locals, params }) => {
+	if (!locals.pb.authStore.isValid) {
+		throw error(401, 'Unauthorized');
+	}
+
+	try {
+		const list = structuredClone(await locals.pb.collection('lists').getOne(params.listId)) as {
+			id: string;
+			name: string;
+			isTemplate: boolean;
+			user: string;
+		};
+
+		if (locals.user?.id === list.user) {
+			return { list };
+		} else {
+			throw error(403, 'Forbidden');
+		}
+	} catch (err) {
+		console.error(err);
+		throw err;
+	}
+};
+
+export const actions: Actions = {
+	updateList: async ({ request, locals, params }) => {
+		const values = await request.formData();
+		const name = values.get('name') as string;
+		const isTemplate = values.has('isTemplate');
+
+		try {
+			await locals.pb.collection('lists').update(params.listId, { name, isTemplate });
+		} catch (err) {
+			console.error(err);
+			throw err;
+		}
+		throw redirect(303, `/lists/${params.listId}`);
+	},
+	deleteList: async ({ locals, params }) => {
+		try {
+			await locals.pb.collection('lists').delete(params.listId);
+		} catch (err) {
+			console.error(err);
+			throw err;
+		}
+		throw redirect(303, `/lists`);
+	}
+};
---
+++ src/routes/lists/[listId]/edit/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Title from '$lib/components/Title.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<form action="?/updateList" method="POST">
+	<Title title={'Edit List ' + data.list.name} />
+	<div class="form-control gap-2 mb-4">
+		<input
+			type="text"
+			name="name"
+			placeholder="Name"
+			value={data.list.name}
+			class="input input-bordered"
+		/>
+		<label for="isTemplate">Is Template?</label>
+		<input
+			type="checkbox"
+			name="isTemplate"
+			checked={data.list.isTemplate}
+			class="input input-bordered"
+		/>
+		<button class="btn btn-primary">Update list</button>
+		<button formaction="?/deleteList" class="btn bg-color-red-500 text-white">Delete list</button>
+	</div>
+</form>
