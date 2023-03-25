4b3dce19b492ad19221b4f7d4e2eeebfdb2dc5ec
Nils Alm <nils.alm@prototyp.se>
2023-03-25 12:41:02 +0100

    Add Register, Log out and Log in pages

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,10 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			pb: import('pocketbase').default;
+			user: import('pocketbase').default['authStore']['model'];
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
 	import '../app.postcss';
+	import { currentUser, pb } from '$lib/pocketbase';
+	import { applyAction, enhance } from '$app/forms';
 </script>
 
 <div class="bg-neutral text-neutral-content">
@@ -9,8 +11,26 @@
 		</div>
 		<div class="navbar-end">
 			<ul class="menu menu-horizontal">
-				<li><a href="/login">Login</a></li>
-				<li><a href="/register">Register</a></li>
+				{#if $currentUser}
+					<li><a href="/">{$currentUser.email}</a></li>
+					<li>
+						<form
+							method="POST"
+							action="/logout"
+							use:enhance={() => {
+								return async ({ result }) => {
+									pb.authStore.clear();
+									await applyAction(result);
+								};
+							}}
+						>
+							<button>Log out</button>
+						</form>
+					</li>
+				{:else}
+					<li><a href="/login">Log in</a></li>
+					<li><a href="/register">Register</a></li>
+				{/if}
 			</ul>
 		</div>
 	</div>
