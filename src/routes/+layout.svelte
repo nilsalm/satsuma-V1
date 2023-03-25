4a1c5d2c34c39dac1827e6e3024ee4015570f325
Nils Alm <nils.alm@prototyp.se>
2023-03-25 09:54:17 +0100

    Add basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,19 @@
 	import '../app.postcss';
 </script>
 
-<slot />
+<div class="bg-neutral text-neutral-content">
+	<div class="max-w-xl mx-auto navbar">
+		<div class="navbar-start">
+			<a href="/" class="btn btn-ghost text-xl">HOME</a>
+		</div>
+		<div class="navbar-end">
+			<ul class="menu menu-horizontal">
+				<li><a href="/login">Login</a></li>
+				<li><a href="/register">Register</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<div class="max-w-xl mx-auto py-8 px-4">
+	<slot />
+</div>
