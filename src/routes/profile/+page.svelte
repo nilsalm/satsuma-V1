766de5856fa5669c7f23a19fe2c04f26be4ea11a
Nils Alm <nils.alm@prototyp.se>
2023-04-25 21:35:24 +0200

    Add a modal blocker to delete category

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -5,6 +5,16 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	export let form;
+
+	$: modalCategoryId = '';
+
+	$: if (form?.success && modalCategoryId != '') closeModal();
+
+	function closeModal() {
+		modalCategoryId = '';
+		if (form) form.success = false;
+	}
 </script>
 
 <div class="mb-4 w-full">
@@ -21,16 +31,40 @@
 					<div class="text-gray-800 font-bold text-xl">{category.name}</div>
 					<div class="flex">
 						<!-- <a href="/categories/{category.id}/edit" class="btn btn-primary mr-2">Edit</a> -->
-
-						<form action="?/deleteCategory" method="POST" use:enhance>
-							<input type="text" name="id" class="hidden" value={category.id} />
-							<div class="w-20">
-								<Button text="Delete" />
-							</div>
-						</form>
+						<Button
+							text="Delete"
+							onClick={() => {
+								modalCategoryId = category.id;
+							}}
+						/>
 					</div>
 				</div>
 			</div>
 		{/each}
 	{/if}
 </div>
+
+{#if modalCategoryId != ''}
+	<div
+		class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex justify-center items-center"
+	>
+		<div class="bg-white rounded-lg shadow-lg p-4 mx-2">
+			<div class="text-gray-800 font-bold text-xl">
+				Are you sure you want to delete this category and all it's items?
+			</div>
+			<div class="flex justify-center mt-4">
+				<form action="?/deleteCategory" method="POST" on:submit|preventDefault use:enhance>
+					<input type="text" name="id" class="hidden" value={modalCategoryId} />
+					<div class="flex justify-between gap-2">
+						<div class="w-20">
+							<Button text="Yes" />
+						</div>
+						<div class="w-20">
+							<Button text="No" onClick={closeModal} />
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+{/if}
