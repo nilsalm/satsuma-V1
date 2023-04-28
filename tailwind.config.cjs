c19012b3cfb6624a02acd7f3ea114fd93a562910
Nils Alm <nils.alm@prototyp.se>
2023-04-28 17:22:05 +0200

    Rollback to npm

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,3 @@
-const typography = require('@tailwindcss/typography');
-const forms = require('@tailwindcss/forms');
-
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
@@ -8,9 +5,7 @@ const config = {
 		extend: {
 			colors: { primary: '#BEAEE2', secondary: '#F7DBF0', accent: '#CDF0EA', neutral: '#F9F9F9' }
 		}
-	},
-
-	plugins: [forms, typography]
+	}
 };
 
 module.exports = config;
