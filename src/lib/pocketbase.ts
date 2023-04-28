76e9a787c0d751d85aee0324b8a8e20c6b128f24
Nils Alm <nils.alm@prototyp.se>
2023-04-28 22:15:30 +0200

    Revert "use dotenv for env"
    This reverts commit 82e2702db651311181e8f099d08e3a23f18bdb31.

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,10 +1,7 @@
-// import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
+import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
 
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const pb = new PocketBase(process.env['PUBLIC_POCKETBASE_URL']);
+export const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
 
 export const currentUser = writable(pb.authStore.model);
