9247f24bc18d5d3d656964c625a0bb7c0d2d60d6
Nils Alm <nils.alm@prototyp.se>
2023-05-01 20:08:32 +0200

    Fixed aspect ratio

--- src/lib/components/List.svelte
+++ src/lib/components/List.svelte
@@ -10,7 +10,7 @@
 	href={`/lists/${list.id}`}
 	class="h-full rounded-lg {list.isTemplate ? 'bg-secondary' : 'bg-primary'} shadow-lg"
 >
-	<div class=" w-24 h-24 max-h-24 max-w-24 text-center m-4 flex flex-col justify-center relative">
+	<div class="aspect-square w-24 text-center m-4 flex flex-col justify-center relative">
 		<p class="font-semibold text-neutral">{list.name}</p>
 		{#if list.isTemplate === true}
 			<p class="text-xs text-gray-500 font-light top-0 right-0 absolute">Template</p>
