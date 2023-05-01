3b1b1c28590ddbadf466961b898a420110c5455a
Nils Alm <nils.alm@prototyp.se>
2023-05-01 20:04:07 +0200

    Sticker header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,21 +4,21 @@
 	import { applyAction, enhance } from '$app/forms';
 </script>
 
-<div class="bg-primary text-neutral h-12">
+<div class="bg-primary text-neutral h-12 sticky top-0 p-1">
 	<div class="max-w-xl mx-auto flex justify-between">
-		<div class="hover:bg-secondary p-2 m-1 rounded hover:bg-opacity-40">
+		<div class="hover:bg-secondary p-1 m-1 my-auto rounded hover:bg-opacity-40">
 			<a href={$currentUser ? '/lists' : '/'} class=" text-xl">🍊</a>
 		</div>
 
-		<ul class="flex">
+		<ul class="flex my-auto">
 			{#if $currentUser}
-				<li class="hover:bg-secondary p-2 m-1 rounded hover:bg-opacity-40">
+				<li class="hover:bg-secondary p-1 m-1 rounded hover:bg-opacity-40">
 					<a class="text-sm" href="/lists">Lists</a>
 				</li>
-				<li class="hover:bg-secondary p-2 m-1 rounded hover:bg-opacity-40">
+				<li class="hover:bg-secondary p-1 m-1 rounded hover:bg-opacity-40">
 					<a class="text-sm" href="/profile">Profile</a>
 				</li>
-				<li class="hover:bg-secondary rounded p-2 m-1 hover:bg-opacity-40">
+				<li class="hover:bg-secondary rounded p-1 m-1 hover:bg-opacity-40">
 					<form
 						method="POST"
 						action="/logout"
@@ -33,10 +33,10 @@
 					</form>
 				</li>
 			{:else}
-				<li class="hover:bg-secondary p-2 m-1 rounded hover:bg-opacity-40">
+				<li class="hover:bg-secondary p-1 m-1 rounded hover:bg-opacity-40">
 					<a class="text-sm" href="/login">Log in</a>
 				</li>
-				<li class="hover:bg-secondary p-2 m-1 rounded hover:bg-opacity-40">
+				<li class="hover:bg-secondary p-1 m-1 rounded hover:bg-opacity-40">
 					<a class="text-sm" href="/register">Register</a>
 				</li>
 			{/if}
