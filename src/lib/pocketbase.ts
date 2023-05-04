663f8d2e0c06438d57851dca68ba8fc3615e20dc
Nils Alm <nils.alm@prototyp.se>
2023-05-04 09:18:08 +0200

    Add model to /lists

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -4,19 +4,54 @@ import { writable } from 'svelte/store';
 import type { Item } from './models/Item';
 import type { Category } from './models/Category';
 import { deepClone } from './util';
+import type { List } from './models/List';
 
 export const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
 
 export const currentUser = writable(pb.authStore.model);
 
 export async function getItemsQuery(listId: string) {
-	const { items } = await pb.collection('items').getList<Item>(1, 100, {
-		filter: `created >= "2022-01-01 00:00:00" && picked = false && list = "${listId}"`
+	const items = deepClone(
+		await pb.collection('items').getList<Item>(1, 100, {
+			filter: `created >= "2022-01-01 00:00:00" && picked = false && list = "${listId}"`
+		})
+	);
+	return items.map((item) => {
+		return {
+			id: item.id,
+			name: item.name,
+			picked: item.picked,
+			category: item.category,
+			list: item.list,
+			user: item.user
+		} as Item;
 	});
-	return items;
 }
 
 export async function getCategoriesQuery() {
-	const items = deepClone(await pb.collection('categories').getFullList<Category>());
-	return items as Category[];
+	const categories = deepClone(await pb.collection('categories').getFullList<Category>());
+	return categories.map((category) => {
+		return {
+			id: category.id,
+			name: category.name,
+			user: category.user
+		} as Category;
+	});
+}
+
+export async function getListsQuery() {
+	const lists = deepClone(
+		await pb.collection('lists').getFullList<List>({
+			filter: 'created >= "2022-01-01 00:00:00"'
+		})
+	);
+
+	return lists.map((list) => {
+		return {
+			id: list.id,
+			name: list.name,
+			isTemplate: list.isTemplate,
+			user: list.user
+		} as List;
+	});
 }
