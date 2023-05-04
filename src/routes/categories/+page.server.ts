9abfb905fee40e67990be6154492e4b6d6701d30
brokelyn <phil.fhs11@googlemail.com>
2023-05-04 14:34:52 +0200

    Add dummy category page

---
+++ src/routes/categories/+page.server.ts
@@ -0,0 +1,54 @@
+import { getCategoriesQuery } from '$lib/pocketbase';
+import { redirect, type Actions } from '@sveltejs/kit';
+
+export const load = ({ locals }) => {
+	if (!locals.pb.authStore.isValid) {
+		throw redirect(303, '/login');
+	}
+
+	const getCategories = async () => {
+		try {
+			const categories = getCategoriesQuery();
+			return categories;
+		} catch (err) {
+			console.error(err);
+			throw err;
+		}
+	};
+
+	return {
+		categories: getCategories()
+	};
+};
+
+export const actions: Actions = {
+	deleteCategory: async ({ locals, request }) => {
+		const values = await request.formData();
+		const id = String(values.get('id'));
+		let page = 0;
+		let totalPages = 1;
+
+		try {
+			while (page < totalPages) {
+				const itemsRecord = await locals.pb.collection('items').getList(1, 500, {
+					filter: `category = '${id}'`
+				});
+				page = itemsRecord.page;
+				totalPages = itemsRecord.totalPages;
+
+				if (itemsRecord.items.length > 0) {
+					await Promise.all(
+						itemsRecord.items.map(
+							async (item) => await locals.pb.collection('items').delete(item.id)
+						)
+					);
+				}
+			}
+			await locals.pb.collection('categories').delete(id);
+		} catch (e) {
+			console.error(e);
+			return { success: false, error: e };
+		}
+		return { success: true };
+	}
+};
