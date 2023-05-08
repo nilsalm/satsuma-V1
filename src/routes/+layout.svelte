6d52377e82e1ec7b168f2722b134cab5c2599e5d
Nils Alm <nils.alm@prototyp.se>
2023-05-08 14:16:54 +0200

    Add logo images and include as component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,19 @@
 	import { currentUser, pb } from '$lib/pocketbase';
 	import { applyAction, enhance } from '$app/forms';
 	import NavButton from '$lib/components/NavButton.svelte';
+	import Logo from '$lib/components/Logo.svelte';
 </script>
 
 <div class="bg-primary text-neutral h-12 sticky top-0 p-1">
 	<div class="max-w-xl mx-auto flex justify-between">
 		<a
 			href={$currentUser ? '/lists' : '/'}
-			class="hover:bg-secondary p-1 m-1 my-auto rounded hover:bg-opacity-40 text-xl">🍊</a
+			class="hover:bg-secondary p-1 m-1 my-auto rounded hover:bg-opacity-40"
 		>
+			<div class="w-8 h-8">
+				<Logo />
+			</div>
+		</a>
 
 		<div class="flex my-auto">
 			{#if $currentUser}
