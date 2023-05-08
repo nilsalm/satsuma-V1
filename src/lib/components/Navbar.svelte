2ee2b94e96d756ecdf56fdfa6de367b414bf2de8
Philipp Krause <phil.fhs11@googlemail.com>
2023-05-08 14:41:12 +0200

    Redesign navbar  for mobile first design (#44)
    * Add footer navigation bar
    
    * Update list design and breakpoints
    
    * Add dummy category page
    
    * Add some padding to lists page
    
    * Add 3 input modes to item editor
    
    * Adjust the bottom offset to always show whole list
    
    * Add enum to item editor state
    
    * Update name of enum

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,37 @@
+<script>
+	import '../../app.postcss';
+	import { currentUser, pb } from '$lib/pocketbase';
+	import { applyAction, enhance } from '$app/forms';
+	import NavButton from '$lib/components/NavButton.svelte';
+</script>
+
+<div class="bg-primary text-neutral h-12 w-full sticky top-0 p-1 z-10">
+  <div class="max-w-xl mx-auto flex justify-between">
+    <a
+      href={$currentUser ? '/lists' : '/'}
+      class="hover:bg-secondary p-1 m-1 my-auto rounded hover:bg-opacity-40 text-xl">🍊</a
+    >
+
+    <div class="flex my-auto">
+      {#if $currentUser}
+        <form
+          method="POST"
+          action="/logout"
+          use:enhance={() => {
+            return async ({ result }) => {
+              pb.authStore.clear();
+              await applyAction(result);
+            };
+          }}
+        >
+          <button class="hover:bg-secondary rounded py-1 px-2 m-1 hover:bg-opacity-40 text-sm"
+            >Log out</button
+          >
+        </form>
+      {:else}
+        <NavButton href="/login" text="Log in" />
+        <NavButton href="/register" text="Register" />
+      {/if}
+    </div>
+  </div>
+</div>
