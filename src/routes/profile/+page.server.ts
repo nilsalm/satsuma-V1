3eb50e60b41be89ba546fc6ba410f91f68b524d5
brokelyn <phil.fhs11@googlemail.com>
2023-05-09 14:18:27 +0200

    Remove categories from profile

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,33 +1,12 @@
-import { deleteCategoryAndAllItemsQuery, getCategoriesQuery } from '$lib/pocketbase';
-import { redirect, type Actions } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 
 export const load = ({ locals }) => {
 	if (!locals.pb.authStore.isValid) {
 		throw redirect(303, '/login');
 	}
 
-	const getCategories = async () => {
-		try {
-			const categories = await getCategoriesQuery();
-			return categories;
-		} catch (err) {
-			console.error(err);
-			throw err;
-		}
-	};
-
 	return {
 		email: locals.pb.authStore.model.email,
-		username: locals.pb.authStore.model.username,
-		categories: getCategories()
+		username: locals.pb.authStore.model.username
 	};
-};
-
-export const actions: Actions = {
-	deleteCategory: async ({ request }) => {
-		const values = await request.formData();
-		const id = String(values.get('id'));
-		await deleteCategoryAndAllItemsQuery(id);
-		return { success: true };
-	}
 };
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,21 +1,8 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
-	import Button from '$lib/components/Button.svelte';
-	import SubTitle from '$lib/components/SubTitle.svelte';
 	import Title from '$lib/components/Title.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	export let form;
-
-	$: modalCategoryId = '';
-
-	$: if (form?.success && modalCategoryId != '') closeModal();
-
-	function closeModal() {
-		modalCategoryId = '';
-		if (form) form.success = false;
-	}
 </script>
 
 <div class="max-w-xl mx-auto py-8 px-4">
@@ -37,56 +24,4 @@
 			</div>
 		</div>
 	</div>
-
-	<div class="mt-8 mb-4 w-full">
-		<SubTitle title={'My Categories'} />
-	</div>
-
-	<div class="w-full mt-4 flex flex-col items-center">
-		{#if data.categories.length === 0}
-			<div class="text-gray-500 text-center">You don't have any categories yet.</div>
-		{:else}
-			{#each data.categories as category}
-				<div class="w-full bg-neutral rounded-lg shadow-lg p-4 mb-4">
-					<div class="flex justify-between items-center">
-						<div class="text-gray-800 font-bold text-xl">{category.name}</div>
-						<div class="flex w-20">
-							<!-- <a href="/categories/{category.id}/edit" class="btn btn-primary mr-2">Edit</a> -->
-							<Button
-								text="Delete"
-								onClick={() => {
-									modalCategoryId = category.id;
-								}}
-							/>
-						</div>
-					</div>
-				</div>
-			{/each}
-		{/if}
-	</div>
-
-	{#if modalCategoryId != ''}
-		<div
-			class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex justify-center items-center"
-		>
-			<div class="bg-white rounded-lg shadow-lg p-4 mx-2">
-				<div class="text-gray-800 font-bold text-xl">
-					Are you sure you want to delete this category and all it's items?
-				</div>
-				<div class="flex justify-center mt-4">
-					<form action="?/deleteCategory" method="POST" on:submit|preventDefault use:enhance>
-						<input type="text" name="id" class="hidden" value={modalCategoryId} />
-						<div class="flex justify-between gap-2">
-							<div class="w-20">
-								<Button text="Yes" />
-							</div>
-							<div class="w-20">
-								<Button text="No" onClick={closeModal} />
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	{/if}
 </div>
