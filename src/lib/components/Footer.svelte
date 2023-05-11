b4d11e9d052598cb54cb5a486de0beb1f8650e78
Nils Alm <nils.alm@prototyp.se>
2023-05-11 21:03:47 +0200

    Style footer navbar

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,19 +1,18 @@
 <script>
 	import FooterButton from '$lib/components/FooterButton.svelte';
 	import { currentUser } from '$lib/pocketbase';
+	import { IconType } from '$lib/types/IconType';
 	import FooterMainButton from './FooterMainButton.svelte';
-
-	import dummyIcon from '$lib/assets/dummy-icon.svg';
 </script>
 
 {#if $currentUser}
 	<div class="w-full bg-primary fixed bottom-0 h-14">
-		<div class="px-1 max-w-xl h-full mx-auto flex justify-around gap-2 md:gap-4">
-			<FooterButton icon="{dummyIcon}" href="/profile" />
-			<FooterButton icon="{dummyIcon}" href="/categories" />
+		<div class="px-1 max-w-md h-full mx-auto flex justify-around gap-2 md:gap-4">
+			<FooterButton icon={IconType.List} href="/lists" title="Lists" />
+			<FooterButton icon={IconType.Template} href="/templates" title="Templates" />
 			<FooterMainButton />
-			<FooterButton icon="{dummyIcon}" href="/lists" />
-			<FooterButton icon="{dummyIcon}" href="/templates" />
+			<FooterButton icon={IconType.Category} href="/categories" title="Categories" />
+			<FooterButton icon={IconType.Profile} href="/profile" title="Profile" />
 		</div>
 	</div>
 	<div class="h-14 md:h-16 w-full" />
--- src/lib/components/FooterButton.svelte
+++ src/lib/components/FooterButton.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import type { IconType } from '$lib/types/IconType';
+	import Icon from '$lib/components/Icon.svelte';
+	import { colors } from '$lib/util';
 
 	export let href: string;
-	export let icon: string;
+	export let icon: IconType;
+	export let title: string;
 
 	$page.route.id;
 
@@ -11,14 +15,12 @@
 	$: isActive = $page.route.id?.includes(href) || false;
 </script>
 
-<div class="flex flex-col justify-center">
+<div class="flex flex-col justify-center" {title}>
 	<a
 		class="text-sm hover:bg-darker p-1 rounded-lg my-2 duration-200 hover:bg-opacity-40
-		{ isActive ? 'bg-darker bg-opacity-40' : ''}"
+		{isActive ? 'bg-darker bg-opacity-40' : ''}"
 		{href}
 	>
-		<div class="text-2xl md:text-3xl">
-			<img alt="Icon" src={icon} class="w-auto h-auto" />
-		</div>
+		<Icon type={icon} size="32" stroke={colors.neutral} />
 	</a>
 </div>
