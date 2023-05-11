d8a5331fd36377fc356c94e13c2f836057faf919
Nils Alm <nils.alm@prototyp.se>
2023-05-11 11:08:30 +0200

    Add Icon component

---
+++ src/lib/types/IconType.ts
@@ -0,0 +1,13 @@
+export enum IconType {
+	Category = 'category', // https://api.iconify.design/iconamoon:component-light.svg
+	List = 'list', // https://api.iconify.design/iconamoon:menu-burger-horizontal-light.svg
+	Profile = 'profile', // https://api.iconify.design/iconamoon:profile-circle-light.svg
+	Settings = 'settings', // https://api.iconify.design/iconamoon:settings-light.svg
+	ShoppingMode = 'shopping-mode', // https://api.iconify.design/iconamoon:shopping-bag-light.svg
+	PlanningMode = 'planning-mode', // https://api.iconify.design/iconamoon:edit-light.svg
+	SwitchCategory = 'switch-category', // https://api.iconify.design/iconamoon:playlist-shuffle-light.svg
+	Template = 'template', // https://api.iconify.design/octicon:stack-24.svg
+	Invite = 'invite', // https://api.iconify.design/iconamoon:send-light.svg
+	Shared = 'shared', // https://api.iconify.design/iconamoon:link-light.svg
+	Drag = 'drag' // https://api.iconify.design/material-symbols:drag-handle-rounded.svg
+}
