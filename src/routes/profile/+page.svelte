f0953396679311b6b7c99519961d637383435f9e
Nils Alm <ntweber75@gmail.com>
2023-05-17 22:23:31 +0200

    Move log out from topbar to profile page (#72)

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import Button from '$lib/components/Button.svelte';
 	import Title from '$lib/components/Title.svelte';
+	import { pb } from '$lib/pocketbase';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	const logout = () => {
+		pb.authStore.clear();
+		goto('/');
+	};
 </script>
 
 <div class="max-w-xl mx-auto py-8 px-4">
-	<div class="mb-4 w-full">
+	<div class="flex justify-between w-full mb-4">
 		<Title title={'My Profile'} />
+		<div class="w-20">
+			<Button text="Log out" backgroundColor="secondary" textStyle="small" onClick={logout} />
+		</div>
 	</div>
 
-	<div class="flex justify-between flex-wrap gap-2">
+	<div class="flex-col justify-between flex-wrap gap-2">
 		<div class="flex gap-4">
 			<div class="font-bold text-xl text-primary">Email:</div>
 			<div class="text-lg">
