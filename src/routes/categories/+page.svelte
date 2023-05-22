4a25dee8d72c506ef4823dd1e9c9f476c5165be1
Nils Alm <nils.alm@prototyp.se>
2023-05-22 09:28:17 +0200

    Remove old modal

--- src/routes/categories/+page.svelte
+++ src/routes/categories/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
-	import { isPlanModeActive } from '$lib/stores/mode';
 	import Button from '$lib/components/Button.svelte';
 	import Title from '$lib/components/Title.svelte';
 	import type { PageData } from './$types';
@@ -9,16 +7,6 @@
 	import { colors } from '$lib/util';
 
 	export let data: PageData;
-	export let form;
-
-	$: modalCategoryId = '';
-
-	$: if (form?.success && modalCategoryId != '') closeModal();
-
-	function closeModal() {
-		modalCategoryId = '';
-		if (form) form.success = false;
-	}
 </script>
 
 <div class="max-w-xl mx-auto pb-8 px-4">
@@ -45,29 +33,4 @@
 			</div>
 		{/if}
 	</div>
-
-	{#if modalCategoryId != ''}
-		<div
-			class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex justify-center items-center"
-		>
-			<div class="bg-white rounded-lg shadow-lg p-4 mx-2">
-				<div class="text-gray-800 font-bold text-xl">
-					Are you sure you want to delete this category and all it's items?
-				</div>
-				<div class="flex justify-center mt-4">
-					<form action="?/deleteCategory" method="POST" on:submit|preventDefault use:enhance>
-						<input type="text" name="id" class="hidden" value={modalCategoryId} />
-						<div class="flex justify-between gap-2">
-							<div class="w-20">
-								<Button text="Yes" />
-							</div>
-							<div class="w-20">
-								<Button text="No" onClick={closeModal} />
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	{/if}
 </div>
