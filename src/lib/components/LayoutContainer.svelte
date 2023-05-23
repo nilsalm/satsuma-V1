d9cd0782eaa875386ef6311065fb9d5b50cb5f15
brokelyn <phil.fhs11@googlemail.com>
2023-05-23 14:57:34 +0200

    Add conditon for padding to layoutcontainer

--- src/lib/components/LayoutContainer.svelte
+++ src/lib/components/LayoutContainer.svelte
@@ -1,3 +1,7 @@
-<div class="max-w-xl mx-auto mt-4 md:mt-8 mb-8 px-4 md:px-0">
+<script lang="ts">
+	export let withPadding: boolean = true;
+</script>
+
+<div class="max-w-xl mx-auto mt-4 md:mt-8 mb-8 {withPadding ? 'px-4': ''}">
 	<slot />
 </div>
