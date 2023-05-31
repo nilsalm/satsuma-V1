46448a986ec0badc8cbfa9373dff905cab5f594e
brokelyn <phil.fhs11@googlemail.com>
2023-05-31 15:02:37 +0200

    Remove buttons from footer and add category button to profile

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -9,9 +9,7 @@
 	<div class="w-full bg-primary fixed bottom-0 h-14">
 		<div class="px-1 max-w-md h-full mx-auto flex justify-around gap-2 md:gap-4">
 			<FooterButton icon={IconType.List} href="/lists" />
-			<FooterButton icon={IconType.Template} href="/templates" />
 			<FooterMainButton />
-			<FooterButton icon={IconType.Category} href="/categories" />
 			<FooterButton icon={IconType.Profile} href="/profile" />
 		</div>
 	</div>
