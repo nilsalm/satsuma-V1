68024805bf99e44d49a98c726fdf53f4242e086d
Philipp Krause <phil.fhs11@googlemail.com>
2023-05-31 17:47:06 +0200

    Remove buttons from footer and add category button to profile (#97)

--- src/routes/templates/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import LayoutContainer from '$lib/components/LayoutContainer.svelte';
-	import Title from '$lib/components/Title.svelte';
-</script>
-
-<LayoutContainer>
-	<Title title="Templates" />
-</LayoutContainer>
