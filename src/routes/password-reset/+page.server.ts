9cb945882df7d53e75483c7a9429724b1065ea17
Philipp Krause <phil.fhs11@googlemail.com>
2023-05-31 15:24:58 +0200

    68 add reset password form (#94)
    * Add password reset page
    
    * Refactor title out of the form
    
    * Remove console log
    
    * Fix typo
    
    * Update text messages

---
+++ src/routes/password-reset/+page.server.ts
@@ -0,0 +1,24 @@
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+
+export const actions: Actions = {
+	default: async ({ locals, request }) => {
+		if (locals.user) {
+			throw redirect(303, '/lists');
+		}
+
+		const data = Object.fromEntries(await request.formData()) as {
+			email: string;
+		};
+
+		data.email = data.email.trim();
+
+		try {
+			await locals.pb.admins.requestPasswordReset(data.email);
+		} catch (e) {
+			return fail(400, { data, incorrect: true });
+		}
+		
+    return { success: true, email: data.email };
+	}
+};
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -10,6 +10,10 @@
 </script>
 
 <LayoutContainer>
+	<div class="w-full mb-4">
+		<Title title="Register new account" />
+	</div>
+
 	<form
 		method="POST"
 		class="card"
@@ -20,10 +24,6 @@
 			};
 		}}
 	>
-		<div class="w-full mb-4">
-			<Title title="Register new account" />
-		</div>
-
 		<div class="flex flex-col gap-4">
 			<input
 				type="text"
@@ -43,12 +43,14 @@
 				placeholder="Confirm Password"
 				class="bg-neutral px-4 text-md text-gray-700 border-2 border-gray-700 font-semibold rounded h-12 shadow-sm"
 			/>
+
+			<Button text="Register" />
+
 			{#if form?.incorrect}
 				<p class="text-center text-red-500">We had trouble creating your account.</p>
 			{:else if form?.passwordMatchError}
 				<p class="text-center text-red-500">Passwords don't match.</p>
 			{/if}
-			<Button text="Register" />
 			<p class="text-center">
 				Already have an account?
 				<a href="/login" class="underline">Log in!</a>
