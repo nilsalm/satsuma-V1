e69b31f9a728d13fbbf76f13f9b144d6617eeb02
Nils Alm <nils.alm@prototyp.se>
2024-01-23 20:23:16 +0100

    change available actions for shop and plan mode

--- src/lib/components/Item.svelte
+++ src/lib/components/Item.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import type { Item } from '$lib/models/Item';
-	import { updateItemQuery } from '$lib/pocketbase';
+	import { deleteItemQuery, updateItemQuery } from '$lib/pocketbase';
 	import { isPlanModeActive } from '$lib/stores/mode';
 	import { createEventDispatcher } from 'svelte';
 
@@ -25,51 +25,75 @@
 		dispatch('updated', { item });
 		await updateItemQuery(item);
 	}
+	async function pickItem() {
+		item.picked = true;
+		dispatch('updated', { item });
+		await updateItemQuery(item);
+	}
+	async function deleteItem() {
+		item.picked = true; // simple workaround to hide the local item
+		dispatch('updated', { item });
+		await deleteItemQuery(item);
+	}
 </script>
 
 <div
 	class="border-b-2 last:border-0 border-secondary rounded-sm px-2 mt-1
 	{item.picked && 'opacity-50'}"
 >
+	<!-- // TODO breaks for unreasonable long item names -->
 	<div class="flex flex-row justify-between items-center">
-		<button class="flex gap-4 py-2 flex-grow mr-8" on:click={togglePickedItem}>
+		<div class="flex gap-4 py-2 flex-grow mr-8">
 			<div class="w-4 text-right text-gray-700">{item.quantity}</div>
 			<div
-				class="text-gray-700 text-left font-semibold sm:w-[360px] truncate whitespace-nowrap
+				class="text-gray-700 text-left font-semibold sm:w-[310px] truncate whitespace-nowrap
 					{item.picked && 'line-through'}"
 			>
 				{item.name}
 			</div>
-		</button>
+		</div>
 
 		{#if !item.picked}
 			<div class="flex gap-2">
-				<button
-					class=" text-gray-700 font-semibold px-2 disabled:opacity-30 border-2 border-primary rounded"
-					disabled={item.quantity <= 1}
-					on:click={decreaseItem}
-				>
-					-
-				</button>
-
-				<button
-					class="text-gray-700 font-semibold px-2 border-2 border-primary rounded"
-					on:click={increaseItem}
-				>
-					+
-				</button>
-
 				{#if $isPlanModeActive}
-						<form action="?/setCategory" method="POST" use:enhance>
-							<button
-								class="text-gray-700 font-semibold px-2 disabled:opacity-30 border-2 border-primary rounded"
-								disabled={!newCategoryId}
-							>
-								🔀
-							</button>
-							<input type="hidden" name="id" value={item.id} />
-							<input type="hidden" name="category" value={newCategoryId} />
-						</form>
+					<button
+						class=" text-gray-700 font-semibold px-2 disabled:opacity-30 border-2 border-primary rounded"
+						disabled={item.quantity <= 1}
+						on:click={decreaseItem}
+					>
+						-
+					</button>
+
+					<button
+						class="text-gray-700 font-semibold px-2 border-2 border-primary rounded"
+						on:click={increaseItem}
+					>
+						+
+					</button>
+
+					<form action="?/setCategory" method="POST" use:enhance>
+						<button
+							class="text-gray-700 font-semibold px-2 disabled:opacity-30 border-2 border-primary rounded"
+							disabled={!newCategoryId}
+						>
+							🔀
+						</button>
+						<input type="hidden" name="id" value={item.id} />
+						<input type="hidden" name="category" value={newCategoryId} />
+					</form>
+					<button
+						class="text-gray-700 font-semibold px-2 border-2 border-primary rounded"
+						on:click={deleteItem}
+					>
+						❌
+					</button>
+				{:else}
+					<button
+						class="text-gray-700 font-semibold px-2 border-2 border-primary rounded"
+						on:click={pickItem}
+					>
+						✅
+					</button>
 				{/if}
 			</div>
 		{/if}
